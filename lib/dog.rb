83738d6c257642bc1d5d950707a4e2cb0c0101b9
Aaron <ztoplin@yahoo.com>
2022-11-25 15:28:10 -0500

    commit tree bf3f7f044c0b24c3d2435f8592b5b122cfa4f38e

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,41 @@
+# class Dog
+
+#     def name=(dog_name)
+#         @this_dogs_name = dog_name
+#     end
+
+#     def name
+#         @this_dogs_name
+#     end
+
+
+# end
+
+# lassie = Dog.new 
+# lassie.name = 'Lassie'
+
+# puts lassie.name
+
 class Dog
 
+    # setter method
+    def name=(value)
+        # assign the @name instance variable a value of whatever is passed as an argument
+        @name = value
+    end
+
+    # getter method
+    def name
+        # access the data from the @name instance variable and return it
+        @name
+    end
+
 end
+
+odie = Dog.new
+
+# call the Dog#name= method with a value of 'Odie'
+odie.name = 'Odie'
+# this equivalent to calling odie.name=('Odie')
+
+puts odie.name
